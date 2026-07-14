9f023188904a9c89292d8009a852142d6da257e8
lasika <lasika0708@gmail.com>
2026-07-14 09:22:42 +0530

    feat:danceForms

--- JS/details.js
+++ JS/details.js
@@ -1,79 +1,115 @@
-const detailsContainer =
-document.getElementById("detailsContainer");
+const API = "https://6a550134e49d9eb2cc557238.mockapi.io/Dance";
 
 
-// Get dance name from URL       
-// console.log(window)
-const params =
-new URLSearchParams(window.location.search);
+// it is getting the  id from URL
+
+const params = new URLSearchParams(window.location.search);
+
+const id = params.get("id");
+const detailsContainer = document.getElementById("detailsContainer");
+const loader = document.getElementById("loader");
 
 
-// brings encoded url  to normal texttt form.. 
-const danceName =
-decodeURIComponent(
-    params.get("dance")
-);
+// Fetch all dances and find matching id
+fetch(API)
+
+.then(response => response.json())
+
+.then(dances => {
 
 
+    const dance = dances.find(item => item.id == id);
 
-async function fetchDanceDetails(){
-
-
-    try{
-        const response = await fetch(
-            `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(danceName)}`
-        );
-
-
-        if(!response.ok){
-            throw new Error("Dance not found");
-        }
-
-
-        const data =
-        await response.json();
-
-        displayDetails(data);
-    }
-
-    catch(error){
-        detailsContainer.innerHTML = `
-
-            <h2>
-            Unable to fetch dance details
-            </h2>
-
-            <p>
-            ${error.message}
-            </p>
-        `;
-
+    if(!dance){
+        throw new Error("Dance not found");
 
     }
-
-
-}
-function displayDetails(dance){
-    const image =
-    dance.thumbnail
-    ? dance.thumbnail.source
-    : "https://via.placeholder.com/600";
+    loader.style.display = "none";
 
 
 
     detailsContainer.innerHTML = `
-        <img src="${image}">
+
+
+        <img 
+        src="${dance.image}"
+        alt="${dance.name}">
+
 
         <h2>
-        ${dance.title}
+            ${dance.name}
         </h2>
 
+
+        <h3>
+            Origin : ${dance.origin}
+        </h3>
+
+
+        <h3>
+            Category : ${dance.category}
+        </h3>
+
+
         <p>
-
-        ${dance.extract}
-
+            ${dance.description}
         </p>
-    `;
-}
 
-fetchDanceDetails();
+        <br>
+        <p>
+            <strong>History:</strong>
+        </p>
+
+
+        <p>
+            ${dance.history}
+        </p>
+
+        <button id="backBtn">
+            Back
+        </button>
+
+
+    `;
+
+
+
+    document.getElementById("backBtn") .addEventListener("click",()=>{
+
+        window.location.href="index.html";
+    });
+
+
+
+})
+
+.catch(error=>{
+
+
+    console.log(error);
+
+
+    loader.style.display="none";
+
+
+    detailsContainer.innerHTML = `
+
+        <h2>
+            Dance Not Found
+        </h2>
+
+        <button id="backBtn">
+            Back
+        </button>
+
+    `;
+
+
+    document
+    .getElementById("backBtn")
+    .addEventListener("click",()=>{
+
+
+        window.location.href="index.html";
+    });
+});
